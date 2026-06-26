97fb23745c2dfe0bb76489c6c26f815c204c1bea
Ekaterina <steffimalleer@gmail.com>
2026-06-26 07:54:53 +0300

    Код для программы написан

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class DownloadSimulator
+{
+    public event Action<int> ProgressChanged;
+
+    public void Download()
+    {
+        int progress = 0;
+
+        while (progress <= 100)
+        {
+            ProgressChanged?.Invoke(progress);
+            Thread.Sleep(250);
+            progress += 10;
+        }
+    }
+}
+
+class Program
+{
+    static void ShowProgress(int value)
+    {
+        Console.WriteLine(value + "%");
+    }
+
+    static void Main()
+    {
+        DownloadSimulator d = new DownloadSimulator();
+
+        d.ProgressChanged += ShowProgress;
+
+        d.Download();
+
+        Console.WriteLine("Готово");
+    }
+}
